3aea01f519be92244a09a1a619e587f67304177c
TimK70 <tkrass1@gmail.com>
2022-12-29 05:01:31 +0000

    Project work

---
+++ VPC.tf
@@ -0,0 +1,14 @@
+#------------VPC.tf file----------
+data "aws_vpc" "default" {
+  default = true
+}
+
+#---We need to create three new subnets in the default vpc
+resource "aws_default_subnet" "project_public_subnets" {
+  availability_zone = element(var.az, count.index)
+  count             = 3
+  force_destroy     = true
+  tags = {
+    Name = "Project Public Subnet ${count.index + 1}"
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,14 @@
+#-------------project providers.tf file-------------
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.48.0"
+    }
+  }
+}
+#2-This will be our default region. If you're using an IDE other than Cloud9, you will
+# put your access_key and secret_key here, below region =.
+provider "aws" {
+  region = "us-west-2"
+}
