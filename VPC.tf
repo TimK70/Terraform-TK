6cf72d527ed59dd2961eaec914a6cb16b4678333
TimK70 <tkrass1@gmail.com>
2022-12-30 05:55:41 +0000

    Updated project..

--- VPC.tf
+++ VPC.tf
@@ -5,7 +5,7 @@ data "aws_vpc" "default" {
 
 #---We need to create three new subnets in the default vpc
 resource "aws_default_subnet" "project_public_subnets" {
-  availability_zone = element(var.az, count.index)
+  availability_zone = element(var.azs, count.index)
   count             = 3
   force_destroy     = true
   tags = {
--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "ec2_instance_type" {
 variable "ami_id" {
   type        = string
   description = "AMI of our instance in us-west-2"
-  default     = "ami-0b5eea76982371e91"
+  default     = "ami-0ceecbb0f30a902a6"
 }
 
 variable "instance_count" {
@@ -38,7 +38,7 @@ variable "cidr_blocks" {
   default = ["0.0.0.0/0"]
 }
 
-variable "az" {
+variable "azs" {
   type        = list(string)
   description = "Availability Zones for instances"
   default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
@@ -48,4 +48,8 @@ variable "public_subnets_cidrs" {
   type        = list(string)
   description = "CIDR's for Public Subnets"
   default     = []
+}
+
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
 }
